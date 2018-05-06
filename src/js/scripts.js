c57a3eaf94fc766d8586231992faf14b5a6b766f
friesam <frieda.musa@subccontrol.com>
2018-05-06 06:42:04 -0230

    working on primary pages.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,26 @@
-// Empty JS for your own code to be here
+// Empty JS for you<p id="demo">JavaScript can change HTML content.</p>
+//window.alert("Welcome to Robert Williamson's Photography");
+
+
+    function openNewsletterPopup() {
+      setTimeout( function() {
+        $('#newsletter').fancybox({wrapCSS: 'newsletter_popup', padding: [40, 40, 40, 40], scrolling: false, maxWidth: 500, minWidth: 250});
+        $('#newsletter').trigger('click');
+      },
+      8*1000);
+    }
+
+    $(function() {
+      var popup = $.cookie('popup');
+      var cookie_enabled = true;
+
+      if (cookie_enabled && popup == 'open') {
+          return false;
+      } else if (false || $(window).width() > 768 || $(window).width() == 0) {
+        openNewsletterPopup();
+      }
+      if (cookie_enabled) {
+        $.cookie('popup', 'open', { expires: 14 });
+      }
+    });
+
